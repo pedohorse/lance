e9fec50376aa33e37688a5ea333807d2abb20f38
pedohorse <xapkohheh@gmail.com>
2019-11-10 01:17:34 -0500

    giving proper error info

--- runtests.py
+++ runtests.py
@@ -8,16 +8,17 @@ import time
 if __name__ == '__main__':
     results = {}
     for TestClass in lancetests.testClassList:
+        #if TestClass.__name__ != 'AddRemoveFoldersTest':continue
         _starttime = time.time()
         try:
             TestClass().run()
             results[TestClass] = {'passed': True, 'time': time.time() - _starttime}
         except Exception as e:
-            results[TestClass] = {'passed': False, 'time': time.time() - _starttime, 'error': {'traceback_string': traceback.extract_stack(), 'exeption': e}}
+            results[TestClass] = {'passed': False, 'time': time.time() - _starttime, 'error': {'traceback_string': ''.join(traceback.format_exc()), 'exeption': e}}
 
     print('\n\nAll tests finished!\n\n')
     for TestClass, result in results.items():
         print('{passed} "{classname}" in {time:.3f}'.format(classname=TestClass.__name__, passed='Passed' if result['passed'] else 'Failed', time=result.get('time', -1)))
         if not result['passed']:
-            print(''.join(results['error'].format()))
+            print(result['error']['traceback_string'])
             print('\n')
