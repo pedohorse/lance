cb8591537d3b5bcb4ff7378a6cc203536516188e
pedohorse <xapkohheh@gmail.com>
2019-11-10 01:18:19 -0500

    testing folder reassignment

--- lancetests/test01.py
+++ lancetests/test01.py
@@ -14,6 +14,12 @@ class AddRemoveFoldersTest(TestBase):
             self.srv = Server(os.path.join(self.test_root_path(), 'srv', 'config'), os.path.join(self.test_root_path(), 'srv', 'data'))
             self.cl0 = Server(os.path.join(self.test_root_path(), 'cl0', 'config'), os.path.join(self.test_root_path(), 'cl0', 'data'))
             self.cl1 = Server(os.path.join(self.test_root_path(), 'cl1', 'config'), os.path.join(self.test_root_path(), 'cl1', 'data'))
+            self.cl2 = Server(os.path.join(self.test_root_path(), 'cl2', 'config'), os.path.join(self.test_root_path(), 'cl2', 'data'))
+
+            srvid = self.srv.syncthingHandler.myId()
+            cl0id = self.cl0.syncthingHandler.myId()
+            cl1id = self.cl1.syncthingHandler.myId()
+            cl2id = self.cl2.syncthingHandler.myId()
 
             fpath0s = os.path.join(self.test_root_path(), 'folder0_s')
             fpath1s = os.path.join(self.test_root_path(), 'folder1_s')
@@ -27,55 +33,80 @@ class AddRemoveFoldersTest(TestBase):
 
             self.cl0.syncthingHandler.setServerSecret('wowsecret2').result()
             self.cl1.syncthingHandler.setServerSecret('wowsecret2').result()
+            self.cl2.syncthingHandler.setServerSecret('wowsecret2').result()
             self.srv.syncthingHandler.setServerSecret('wowsecret2').result()
 
-            self.srv.syncthingHandler.addServer(self.srv.syncthingHandler.myId()).result()
-            self.srv.syncthingHandler.addDevice(self.cl0.syncthingHandler.myId()).result()
-            self.srv.syncthingHandler.addDevice(self.cl1.syncthingHandler.myId()).result()
-            self.cl0.syncthingHandler.addServer(self.srv.syncthingHandler.myId()).result()
-            self.cl1.syncthingHandler.addServer(self.srv.syncthingHandler.myId()).result()
+            self.srv.syncthingHandler.addServer(srvid).result()
+            self.srv.syncthingHandler.addDevice(cl0id).result()
+            self.srv.syncthingHandler.addDevice(cl1id).result()
+            self.srv.syncthingHandler.addDevice(cl2id).result()
+            ###
+            self.cl0.syncthingHandler.addServer(srvid).result()
+            self.cl1.syncthingHandler.addServer(srvid).result()
+            self.cl2.syncthingHandler.addServer(srvid).result()
 
-            self.srv.syncthingHandler.addFolder(fpath0s, 'folder_A', [self.cl0.syncthingHandler.myId()]).result()
-            self.srv.syncthingHandler.addFolder(fpath1s, 'floder_B', [self.cl1.syncthingHandler.myId()]).result()
-            self.srv.syncthingHandler.addFolder(fpath1s, 'floder_C', [self.cl1.syncthingHandler.myId()]).result()
+            fidA = self.srv.syncthingHandler.addFolder(fpath0s, 'folder_A', [cl0id]).result()
+            fidB = self.srv.syncthingHandler.addFolder(fpath1s, 'folder_B', [cl1id]).result()
+            fidC = self.srv.syncthingHandler.addFolder(fpath2s, 'folder_C', [cl2id]).result()
 
             for i in range(3):
                 self.srv.start()
                 self.cl0.start()
                 self.cl1.start()
+                self.cl2.start()
 
                 time.sleep(10)
                 s0 = 'f0 random information %s' % ''.join([random.choice(string.printable) for _ in range(16)])
                 s1 = 'f1 randomer informationes %s' % ''.join([random.choice(string.printable) for _ in range(16)])
+                s2 = 'f2 even randomer informationes %s' % ''.join([random.choice(string.printable) for _ in range(16)])
                 with open(os.path.join(fpath0s, 'f0_test.txt'), 'w') as f:
                     f.write(s0)
                 with open(os.path.join(fpath1s, 'f1_test.txt'), 'w') as f:
                     f.write(s1)
+                with open(os.path.join(fpath2s, 'f2_test.txt'), 'w') as f:
+                    f.write(s2)
 
-                time.sleep(15) # + i*999999)
+                time.sleep(25)
 
                 with open(os.path.join(fpath0s, 'f0_test.txt'), 'r') as f:
                     ss0 = f.read()
                 with open(os.path.join(fpath1s, 'f1_test.txt'), 'r') as f:
                     ss1 = f.read()
+                with open(os.path.join(fpath2s, 'f2_test.txt'), 'r') as f:
+                    ss2 = f.read()
 
-                with open(os.path.join(self.test_root_path(), 'cl0', 'data', 'le folder 0', 'f0_test.txt'), 'r') as f:
+                with open(os.path.join(self.test_root_path(), 'cl0', 'data', 'folder_A', 'f0_test.txt'), 'r') as f:
                     assert ss0 == f.read(), 'c0 folder 0 contents mismatch. test failed. \n%s' % s0
-                with open(os.path.join(self.test_root_path(), 'cl1', 'data', 'la foldero 1', 'f1_test.txt'), 'r') as f:
+                with open(os.path.join(self.test_root_path(), 'cl1', 'data', 'folder_B', 'f1_test.txt'), 'r') as f:
                     assert ss1 == f.read(), 'c1 folder 1 contents mismatch. test failed. \n%s' % s1
+                with open(os.path.join(self.test_root_path(), 'cl2', 'data', 'folder_C', 'f2_test.txt'), 'r') as f:
+                    assert ss2 == f.read(), 'c2 folder 2 contents mismatch. test failed. \n%s' % s2
 
-                print('test 0, iter %d.\n    %s\n    %s\n----\n    %s\n    %s\n' % (i, s0, ss0, s1, ss1))
+                self.srv.syncthingHandler.removeDeviceFromFolder(fidA, cl0id).result()
+                self.srv.syncthingHandler.removeDeviceFromFolder(fidB, cl1id).result()
+                self.srv.syncthingHandler.removeDeviceFromFolder(fidC, cl2id).result()
 
-                os.remove(os.path.join(self.test_root_path(), 'cl1', 'data', 'la foldero 1', 'f1_test.txt'))
-                os.remove(os.path.join(self.test_root_path(), 'cl0', 'data', 'le folder 0', 'f0_test.txt'))
+                self.srv.syncthingHandler.addDeviceToFolder(fidA, cl1id).result()
+                self.srv.syncthingHandler.addDeviceToFolder(fidB, cl2id).result()
+                self.srv.syncthingHandler.addDeviceToFolder(fidC, cl0id).result()
 
-                time.sleep(15)
+                time.sleep(35)
 
-                assert not os.path.exists(os.path.join(fpath0s, 'f0_test.txt')), 'cl0-s file removal not synced'
-                assert not os.path.exists(os.path.join(fpath1s, 'f1_test.txt')), 'cl1-s file removal not synced'
+                assert not os.path.exists(os.path.join(self.test_root_path(), 'cl0', 'data', 'folder_A')), 'FolderA not deleted from cl0'
+                assert not os.path.exists(os.path.join(self.test_root_path(), 'cl1', 'data', 'folder_B')), 'FolderB not deleted from cl1'
+                assert not os.path.exists(os.path.join(self.test_root_path(), 'cl2', 'data', 'folder_C')), 'FolderC not deleted from cl2'
+
+                with open(os.path.join(self.test_root_path(), 'cl1', 'data', 'folder_A', 'f0_test.txt'), 'r') as f:
+                    assert ss0 == f.read(), 'c1 folder 0 contents mismatch. test failed. \n%s' % s0
+                with open(os.path.join(self.test_root_path(), 'cl2', 'data', 'folder_B', 'f1_test.txt'), 'r') as f:
+                    assert ss1 == f.read(), 'c2 folder 1 contents mismatch. test failed. \n%s' % s1
+                with open(os.path.join(self.test_root_path(), 'cl0', 'data', 'folder_C', 'f2_test.txt'), 'r') as f:
+                    assert ss2 == f.read(), 'c3 folder 2 contents mismatch. test failed. \n%s' % s2
+
 
                 self.cl0.stop()
                 self.cl1.stop()
+                self.cl2.stop()
                 self.srv.stop()
 
                 print("EVERYTHING STOPPED! ITERATION %d DONE" % i)
@@ -83,6 +114,15 @@ class AddRemoveFoldersTest(TestBase):
                 self.srv = Server(os.path.join(self.test_root_path(), 'srv', 'config'), os.path.join(self.test_root_path(), 'srv', 'data'))
                 self.cl0 = Server(os.path.join(self.test_root_path(), 'cl0', 'config'), os.path.join(self.test_root_path(), 'cl0', 'data'))
                 self.cl1 = Server(os.path.join(self.test_root_path(), 'cl1', 'config'), os.path.join(self.test_root_path(), 'cl1', 'data'))
+                self.cl2 = Server(os.path.join(self.test_root_path(), 'cl2', 'config'), os.path.join(self.test_root_path(), 'cl2', 'data'))
+
+                self.srv.syncthingHandler.removeDeviceFromFolder(fidA, cl1id).result()
+                self.srv.syncthingHandler.removeDeviceFromFolder(fidB, cl2id).result()
+                self.srv.syncthingHandler.removeDeviceFromFolder(fidC, cl0id).result()
+
+                self.srv.syncthingHandler.addDeviceToFolder(fidA, cl0id).result()
+                self.srv.syncthingHandler.addDeviceToFolder(fidB, cl1id).result()
+                self.srv.syncthingHandler.addDeviceToFolder(fidC, cl2id).result()
         finally:
             try:
                 self.cl0.stop()
@@ -90,6 +130,10 @@ class AddRemoveFoldersTest(TestBase):
                 pass
             try:
                 self.cl1.stop()
+            except:
+                pass
+            try:
+                self.cl2.stop()
             except:
                 pass
             try:
@@ -104,6 +148,10 @@ class AddRemoveFoldersTest(TestBase):
             pass
         try:
             self.cl1.stop()
+        except:
+            pass
+        try:
+            self.cl2.stop()
         except:
             pass
         try:
