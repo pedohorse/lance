a8516034a316866b39257fe9bd833c3d58635947
pedohorse <xapkohheh@gmail.com>
2019-11-13 01:10:18 -0500

    launching detailViewer with a server instance

--- detailViewer_tester.py
+++ detailViewer_tester.py
@@ -1,10 +1,16 @@
 import sys
 from PySide2.QtWidgets import QApplication
 
+from lance.server import Server
 from lance_gui.detailviewer import DetailViewer
 
 if __name__ == '__main__':
     qapp = QApplication(sys.argv)
     wgt = DetailViewer()
+    srv = Server('/tmp/lancetemp/cfg', '/tmp/lancetemp/data')
+    wgt.set_server(srv)
+    srv.start()
     wgt.show()
-    sys.exit(qapp.exec_())
+    res = qapp.exec_()
+    srv.stop()
+    sys.exit(res)
