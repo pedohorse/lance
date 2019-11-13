737d8cb06fc48cefddebabdc069c747ac3eb9de5
pedohorse <xapkohheh@gmail.com>
2019-11-13 01:12:09 -0500

    moved new eventprocessors processing before event processing, so new processors wont miss any events cleaned some trash methods

--- lance/eventqueueeater.py
+++ lance/eventqueueeater.py
@@ -15,7 +15,6 @@ class EventQueueEater(ServerComponent, lance_utils.EventQueueReader):
         # note that there's no _processAsyncMethods cuz we do not have async methods
         # if this class gets any async methods - we'll have to implement double queue waiting...
         while not self._stopped_set():
-            yield
             try:
                 event = self._dequeueEvent(block=True, timeout=10)
             except queue.Empty:
@@ -32,22 +31,15 @@ class EventQueueEater(ServerComponent, lance_utils.EventQueueReader):
                 except queue.Empty:  # can happen due to threading
                     pass
 
-            # update existing processors
-            for ep in self.__eventProcessors:
-                if not ep.is_alive():
-                    self.__eventProcessors.remove(ep)
-                    continue
-                if ep.is_expected_event(event):
-                    ep.add_event(event)
             # create new processors
             while self.__eventProcessorsAddQueue.qsize() > 0:
                 try:
-                    #eventproc_type, event, data = self.__eventProcessorsAddQueue.get_nowait()
-                    #newEventProcessor = eventproc_type(self, event, data)
+                    # eventproc_type, event, data = self.__eventProcessorsAddQueue.get_nowait()
+                    # newEventProcessor = eventproc_type(self, event, data)
                     newEventProcessor = self.__eventProcessorsAddQueue.get_nowait()
                     self.__eventProcessors.append(newEventProcessor)
                     self.__eventProcessorsAddQueue.task_done()
-                    if not newEventProcessor.is_alive():
+                    if isinstance(newEventProcessor, eventprocessor.BaseEventProcessor) and not newEventProcessor.is_alive():
                         newEventProcessor.start()
                 except queue.Empty:
                     pass
@@ -59,13 +51,18 @@ class EventQueueEater(ServerComponent, lance_utils.EventQueueReader):
             except Exception as e:
                 print('something went wrong when creating event processor: %s' % repr(e))
 
+            # update existing processors
+            for ep in self.__eventProcessors:
+                if isinstance(ep, eventprocessor.BaseEventProcessor) and not ep.is_alive():
+                    self.__eventProcessors.remove(ep)
+                    continue
+                if ep.is_expected_event(event):
+                    ep.add_event(event)
+
             self._eventProcessed()
 
     def add_event_processor(self, eventprocessor):
         self.__eventProcessorsAddQueue.put(eventprocessor)
 
     def remove_event_provessor(self, eventprocessor):
-        self.__eventProcessorsRemoveQueue.put(eventprocessor)
-
-    def _event_processing_completed(self, eventprocessor): # TODO: get rid of this? or do we need it?
         self.__eventProcessorsRemoveQueue.put(eventprocessor)
