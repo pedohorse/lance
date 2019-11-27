dce643235742b0c2365553034dce12b8a312e12c
pedohorse <xapkohheh@gmail.com>
2019-11-27 00:56:44 -0500

    a bit more user firendly out

--- runtests.py
+++ runtests.py
@@ -9,16 +9,17 @@ import re
 
 if __name__ == '__main__':
     results = {}
-    filterre = None
+    filterre = '.*'
     if len(sys.argv) > 1:
         try:
             filterre = re.compile(sys.argv[1])
         except Exception as e:
             print('bad regexp: %s' % repr(e))
             sys.exit(1)
-    for TestClass in lancetests.testClassList:
-        if filterre is not None and filterre.match(TestClass.__name__) is None:
-            continue
+    filteredClassList = list(filter(lambda x: filterre.match(x.__name__) is not None, lancetests.testClassList))
+    print('\n\n\t\tTESTS WILL BE RUN:\n\t\t\t{testnames}\n\n'.format(testnames='\n\t\t\t'.join(map(lambda x: x.__name__, filteredClassList))))
+    for TestClass in filteredClassList:
+        print('\n\nrunning test: {testname}\n\n'.format(testname=TestClass.__name__))
         #if TestClass.__name__ != 'AddRemoveFoldersTest':continue
         _starttime = time.time()
         try:
@@ -33,3 +34,4 @@ if __name__ == '__main__':
         if not result['passed']:
             print(result['error']['traceback_string'])
             print('\n')
+    print('\n\n')
